7483f04c4921e3ebdbb91d902dd68cdc125c93df
Mikael Carlsson <micke@radiosyd.se>
2026-09-10 19:20:42 +0200

    Update include_shoplist_sum to support integer

--- HTML/include/include_shoplist_sum.php
+++ HTML/include/include_shoplist_sum.php
@@ -1,36 +1,43 @@
 <?php
+// File: include_shoplist_sum.php
+// Function: Sums the cost for a shopping lists
+// Revision date: 2026-09-10
+// Revised by: Mikael Karlsson
+// This file is distributed under the license:
+// Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+//
+
 class ShoplistPrice {
-	public function ShoplistPriceSum() {
-	
-		require_once('login/auth.php');
-		include('mysql_connect.php');
+    public function ShoplistPriceSum() {
 
-		$owner = $_SESSION['SESS_MEMBER_ID'];
-		$GetPersonal = mysqli_query($connection,"SELECT currency FROM members WHERE member_id = ".$owner."");
-		$personal = mysqli_fetch_assoc($GetPersonal);
+    require_once "login/auth.php";
+    include "mysql_connect.php";
 
-		$GetDataComponentsAll = "SELECT price,order_quantity FROM data WHERE owner = ".$owner." AND order_quantity > 0 ORDER by name ASC";
+    $owner = $_SESSION['SESS_MEMBER_ID'];
+    $GetPersonal = mysqli_query($connection,"SELECT currency FROM members WHERE member_id = ".$owner."");
+    $personal = mysqli_fetch_assoc($GetPersonal);
 
-		$sql_exec = mysqli_Query($connection,$GetDataComponentsAll);
-		while($showDetails = mysqli_fetch_array($sql_exec)) {
-	
-			$price = $showDetails['price'];
-			$quantity = $showDetails['order_quantity'];
+    $GetDataComponentsAll = "SELECT price,order_quantity FROM data WHERE owner = ".$owner." AND order_quantity > 0 ORDER by name ASC";
 
-			$product =  $price * $quantity;
-			$sum[] = $product;
-			
-		}
-		if (isset($sum)) {
-			echo array_sum($sum);
-			echo ' ';
-			echo $personal['currency'];
-		}
-		else {
-			echo '0';
-			echo ' ';
-			echo $personal['currency'];
-		}
-	}
+    $sql_exec = mysqli_Query($connection,$GetDataComponentsAll);
+        while($showDetails = mysqli_fetch_array($sql_exec)) {
+            $price = $showDetails['price'];
+            (int)$quantity = $showDetails['order_quantity']; // quantity is now an integer
+
+            $product =  (float)$price * $quantity; // so we have to casr $price as a float
+            $sum[] = $product;
+
+        }
+        if (isset($sum)) {
+            echo array_sum($sum);
+            echo ' ';
+            echo $personal['currency'];
+        }
+        else {
+            echo '0';
+            echo ' ';
+            echo $personal['currency'];
+        }
+    }
 }
 ?>
