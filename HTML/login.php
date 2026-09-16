d801ab04d3ca9ac6bd8e51477f79420fa1926c36
Mikael Carlsson <micke@radiosyd.se>
2026-09-16 15:41:19 +0200

    Update login.php

--- HTML/login.php
+++ HTML/login.php
@@ -35,7 +35,7 @@
 // END
     echo '<body><div id="wrapper">';
 // Header
-    echo '<div><img src="img/logov2.png" alt="ECDB personal V2" style="width:175px;height:75px;"></div>';
+    echo '<div><img src="img/logov2.png" alt="ecDB personal V2" style="width:175px;height:75px;"></div>';
 // END
 // Main menu
     echo '<div id="menu"><ul>';
