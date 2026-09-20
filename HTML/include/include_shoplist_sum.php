96521895cdf26827448441df21b2dc9b30903f7c
Mikael Carlsson <micke@radiosyd.se>
2026-09-20 19:28:19 +0200

    Update include_shoplist_sum.php

--- HTML/include/include_shoplist_sum.php
+++ HTML/include/include_shoplist_sum.php
@@ -34,7 +34,7 @@ class ShoplistPrice {
         } else {
             $price = $total;
         }
-        echo _("Total cost: ");
+        echo _("Total cost") . ": ";
         echo format_currency($personal['language'],$personal['currency'], $price) ;
     }
 }
